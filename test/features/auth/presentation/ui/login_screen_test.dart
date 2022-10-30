7afb47705062a2a88d52b3b2e5c7a695d9436535
richard <richard@maqe.com>
2022-10-30 21:58:45 +0700

    added login screen test with providerscope

--- test/features/auth/presentation/ui/login_screen_test.dart
+++ test/features/auth/presentation/ui/login_screen_test.dart
@@ -1,36 +1,126 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:multiple_result/multiple_result.dart';
 import 'package:youtube_sample_app/common/widget/widget_key.dart';
+
+import 'package:youtube_sample_app/features/auth/application/iauth_service.dart';
+import 'package:youtube_sample_app/features/auth/presentation/ui/controller/auth_controller.dart';
 import 'package:youtube_sample_app/features/auth/presentation/ui/login_screen.dart';
+import 'package:youtube_sample_app/features/auth/presentation/ui/state/login_state.dart';
+import '../../../../../test_data/login/login_request_test.dart';
+
+
+import 'controller/auth_controller_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<IAuthService>()])
 
 void main() {
+  late IAuthService mockIAuthService;
+  
 
-  testWidgets('login screen ...', (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: LoginScreen(),
-        ),
-      ),
-      
-   );
-    await tester.pumpAndSettle();
-    expect(find.text('Login'), findsNWidgets(3));
-
-    final emailField = find.byKey(emailTextKey);
-    expect(emailField, findsOneWidget);
-    await tester.enterText(emailField, 'demo1@gmail.com');
-
-    final passwordField = find.byKey(passwordTextKey);
-    expect(passwordField, findsOneWidget);
-    await tester.enterText(passwordField, '123456');
-   
-    await tester.tap(find.byKey(btnLoginKey));
-    await tester.pumpAndSettle();
-    expect(find.text('email is empty'), findsNothing);
-    expect(find.text('password is empty'), findsNothing);
-       
+  setUp(() {
+    mockIAuthService = MockIAuthService();
+    
   });
 
-  }
+  group('Login screen test', () {
+
+      testWidgets('login screen with password validation error ', (tester) async {
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: LoginScreen(),
+          ),
+        );
+        await tester.pump();
+        expect(find.text('Login'), findsNWidgets(3));
+
+        final emailField = find.byKey(emailTextKey);
+        expect(emailField, findsOneWidget);
+        await tester.enterText(emailField,'demo@gmial.com');
+
+        final passwordField = find.byKey(passwordTextKey);
+        expect(passwordField, findsOneWidget);
+        await tester.enterText(passwordField,'12345');
+
+        expect(find.text("Don't have a account?"), findsOneWidget);
+        expect(find.byKey(registerNowTextKey),findsOneWidget);
+
+        final loginBtn = find.byKey(btnLoginKey);
+        expect(loginBtn, findsOneWidget);
+
+        await tester.tap(loginBtn);
+        await tester.pumpAndSettle();
+
+        expect(find.text('password is invalid'), findsOneWidget);
+
+    
+      });
+
+    testWidgets('login screen with email and password validation success ', (tester) async {
+       
+        when(mockIAuthService.login(loginRequestTest))
+          .thenAnswer((_) async => const Success(true));
+
+        final mockAuthControllerProvider = StateNotifierProvider.autoDispose<AuthController,LoginState>((ref) {
+          final controller =  AuthController(mockIAuthService, LoginState(const AsyncLoading(),null));
+          return controller;
+        });
+        
+        await tester.pumpWidget(
+          ProviderScope (
+          overrides: [
+            authControllerProvider.overrideWithProvider(mockAuthControllerProvider)
+          ],
+          child: const MaterialApp(
+            home: LoginScreen(),
+          ),
+        ),
+        );
+        await tester.pump();
+        expect(find.text('Login'), findsNWidgets(3));
+
+        final emailField = find.byKey(emailTextKey);
+        expect(emailField, findsOneWidget);
+        await tester.enterText(emailField,loginRequestTest.email);
+
+        final passwordField = find.byKey(passwordTextKey);
+        expect(passwordField, findsOneWidget);
+        await tester.enterText(passwordField,loginRequestTest.password);
+       
+        final loginBtn = find.byKey(btnLoginKey);
+        expect(loginBtn, findsOneWidget);
+
+        await tester.tap(loginBtn);
+        await tester.pumpAndSettle();
+
+        expect(find.text('email is empty'), findsNothing);
+        expect(find.text('password is empty'), findsNothing);
+        expect(find.text('password is invalid'), findsNothing);
+        expect(find.text('email is invalid'), findsNothing);
+
+    
+      });
+
+    testWidgets('login screen icon test', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: LoginScreen(),
+        ),
+      );
+      await tester.pump();  
+
+      expect(find.byIcon(Icons.security), findsOneWidget);
+      expect(find.byIcon(Icons.email), findsOneWidget);
+      expect(find.byIcon(Icons.clear), findsNWidgets(2));
+      expect(find.byType(IconButton), findsNWidgets(2));
+
+
+     });
+
+  });
+
+  
+}
--- test_data/login/login_request_test.dart
+++ test_data/login/login_request_test.dart
@@ -2,4 +2,4 @@
 
 import 'package:youtube_sample_app/features/auth/data/dto/request/login/login_request.dart';
 
-LoginRequest loginRequestTest = const LoginRequest(email: 'demo1@gmail.com', password: '123456');
+LoginRequest loginRequestTest = const LoginRequest(email: 'demo1@gmail.com', password: '123456abc');
