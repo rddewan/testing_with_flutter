c2237e1421381d65bc2797e2fd1c0bd2db70d422
richard <richard@maqe.com>
2022-10-31 11:42:04 +0700

    update script

--- build_ipa.sh
+++ build_ipa.sh
@@ -12,3 +12,5 @@ popd
 pushd $product
 zip -r "ios_tests.zip" "Release-iphoneos" "Runner_iphoneos$dev_target-arm64.xctestrun"
 popd
+
+gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" --device model=iphone11pro,version=14.7,locale=en_US,orientation=portrait
